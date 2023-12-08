33db890ef06ec14c0e71f242fa8c71f6000eb00c
Vince Veselosky <vince@control-escape.com>
2023-12-07 21:02:09 -0500

    Move and update requirements files

--- README.md
+++ README.md
@@ -1,16 +1,34 @@
-# writertools
+# Writer Tools
 
 A Django project for my writer tools website.
 
+## Roadmap
+
 ## Development
 
-After checking out the code, setup your dev environment:
+Check out the code and run:
 
-```sh
-python3.8 -m venv --prompt writertools venv
-. venv/bin/activate
-pip install --upgrade pip
-pip install -e ".[dev]"
--e .env || cp example.env .env
-python manage.py migrate
-```
+`python ./manage.py devsetup`
+
+This will create a virtual environment and install requirements. Note that the version
+of Python used to run `devsetup` will be the one used in your virtual environment.
+
+## Dependency Management
+
+This project includes pip-tools for dependency management. There are two requirements
+files: requirements.in provides the acceptable ranges of packages to install in a
+production environment (or any other environment); requirements-dev.in provides packages
+to install in development environments. Both of these have corresponding "pin" files:
+requirements.txt and requirements-dev.txt.
+
+To add a new dependency, add it to the correct .in file, and then run manage.py pipsync
+to regenerate the pin files and synchronize your current virtual environment with the
+new pin files.
+
+Any arguments passed to manage.py pipsync will be passed through to the underlying
+pip-compile command. For example, to bump to the latest Django patch release use
+manage.py pipsync --upgrade-package django. See the pip-tools docs for complete details.
+
+The pin files are not included in the template repository, but will be generated when
+you run manage.py devsetup. This ensures you will get the latest version of Django and
+related packages when starting a new project.
--- manage.py
+++ manage.py
@@ -14,106 +14,159 @@ import os
 import shutil
 import subprocess
 import sys
+import typing as T
 import venv
 from pathlib import Path
 
-
-def devsetup(rebuild=False):
-    """Set up an environment for local development."""
-    this = Path(sys.argv[0]).resolve(strict=True)
-    venvdir = this.parent / "venv"
-    if rebuild:
-        # Nuke the venv and recreate
-        print("Rebuilding virtualenv")
-        venv.create(str(venvdir), clear=True, with_pip=True, prompt="writertools")
-    elif not venvdir.exists():
-        print("Creating virtualenv")
-        venv.create(str(venvdir), with_pip=True, prompt="writertools")
-
-    python = venvdir / "bin" / "python"
-
-    # Upgrade pip in virtualenv. Need pip >= 21.3.1 for -e with pyproject.toml
-    # 3.8's pip won't cut it.
-    print("Upgrading virtualenv to latest pip")
-    subprocess.run([python, "-m", "pip", "install", "-q", "-U", "pip", "wheel"])
-
-    print("Installing requirements. May take a bit. Grab a coffee.")
-    subprocess.run(
-        [python, "-m", "pip", "install", "-q", "-r", "etc/pip/requirements-dev.txt"]
-    )
-
-    # FIXME HACK support editable genericsite
-    print("HACK Installing editable django-genericsite for development only.")
-    subprocess.run(
-        [
-            python,
-            "-m",
-            "pip",
-            "install",
-            "-q",
-            "-e",
-            "../django-genericsite/",
-        ]
-    )
-
-    # If no .env, copy example.env to .env
-    dotenv = this.parent / ".env"
-    if not dotenv.exists():
-        print("Creating local .env file")
-        shutil.copy(this.parent.joinpath("example.env"), dotenv)
-
-    # Create the dev database
-    print("Applying database migrations")
-    subprocess.run([python, "manage.py", "migrate"])
-
-    print("Dev setup complete. Activate your virtualenv with: . venv/bin/activate")
+PROJECT_DIR = Path(__file__).parent
+base_in = PROJECT_DIR / "requirements.in"
+dev_in = PROJECT_DIR / "requirements-dev.in"
+dev_reqs = PROJECT_DIR / "requirements-dev.txt"
+base_reqs = PROJECT_DIR / "requirements.txt"
 
 
-def upgrade_requirements():
-    this = Path(sys.argv[0]).resolve(strict=True)
-    venvdir = this.parent / "venv"
-    python = venvdir / "bin" / "python"
-    env = os.environ.copy()
-    env.setdefault("CUSTOM_COMPILE_COMMAND", "./manage.py upgrade_requirements")
-    subprocess.run(
-        [
-            python,
-            "-m",
-            "piptools",
-            "compile",
-            "--upgrade",
-            # "--generate-hashes",
-            # "--reuse-hashes",
-            "--no-emit-index-url",
-            "--output-file=requirements.txt",
-            "etc/pip/requirements.in",
-        ],
-        env=env,
-        capture_output=True,
-        check=True,
-    )
-    subprocess.run(
-        [
-            python,
-            "-m",
-            "piptools",
-            "compile",
-            "--upgrade",
-            "--generate-hashes",
-            "--reuse-hashes",
-            "--no-emit-index-url",
-            "--output-file=requirements-test.txt",
-            "etc/pip/requirements-test.in",
-        ],
-        env=env,
-        capture_output=True,
-        check=True,
+def get_django_package() -> str:
+    """A simple hueristic to find the main package for this Django project."""
+    for item in PROJECT_DIR.iterdir():
+        if not item.is_dir():
+            continue
+        if item.joinpath("settings.py").exists():
+            # Found it
+            return item.name
+
+    # If we fall through the loop, there is no settings.py, or this file is in the
+    # wrong place.
+    raise Exception(
+        "Unable to locate a Django settings file (searched */settings.py)."
+        "This script cannot continue without a settings file."
     )
 
 
-def django_command(args=None):
+def get_python_binary_from(VENV: Path) -> str:
+    for maybe in [VENV / "bin" / "python", VENV / "Scripts" / "python.exe"]:
+        if maybe.exists():
+            return str(maybe)
+    raise Exception(f"Unable to locate Python binary in {VENV}")
+
+
+def in_virtualenv() -> bool:
+    """Return true if running inside a virtualenv"""
+    return sys.prefix != sys.base_prefix
+
+
+def create_venv(
+    path: Path, prompt: T.Union[str, None] = None, clear: bool = False
+) -> str:
+    """
+    Create a virtual env in the given path and return a pathlib.Path instance
+    representing the python executable.
+    """
+    venv.create(str(path), clear=clear, with_pip=True, prompt=prompt)
+    return get_python_binary_from(path)
+
+
+def _compile(PYTHON: str, *args):
+    cmd = [
+        PYTHON,
+        "-m",
+        "piptools",
+        "compile",
+        "--resolver=backtracking",
+        "-qq",
+        "--allow-unsafe",
+    ]
+    cmd.extend(args)
+    subprocess.check_call(cmd, cwd=str(PROJECT_DIR))
+
+
+def _pip(PYTHON: str, *args):
+    cmd = [PYTHON, "-m", "pip", "-q"]
+    cmd.extend(args)
+    subprocess.check_call(cmd, cwd=str(PROJECT_DIR))
+
+
+def _sync(PYTHON: str, *args):
+    cmd = [PYTHON, "-m", "piptools", "sync", "-q"]
+    cmd.extend(args)
+    subprocess.check_call(cmd, cwd=str(PROJECT_DIR))
+
+
+def _compile_and_sync(PYTHON: str, args: T.Iterable):
+    """Run pip-compile on requirements.in and requirements-dev.in, then pip-sync"""
+    # Ensure pip and piptools are up to date in the venv
+    print("Checking for the latest pip updates.")
+    _pip(PYTHON, "install", "--upgrade", "pip", "pip-tools")
+
+    # Then, use piptools sync to install requirements
+    print("Checking for requirements changes.")
+    _compile(
+        PYTHON,
+        "-o",
+        str(base_reqs.relative_to(PROJECT_DIR)),
+        *args,
+        str(base_in.relative_to(PROJECT_DIR)),
+    )
+    _compile(
+        PYTHON,
+        "-o",
+        str(dev_reqs.relative_to(PROJECT_DIR)),
+        *args,
+        str(dev_in.relative_to(PROJECT_DIR)),
+    )
+    print("Updating the current virtual environment.")
+    _sync(PYTHON, str(dev_reqs))
+
+
+class Commands:
+    """
+    This class is a container for commands that are not part of Django, but that we want
+    to run occasionally. It exists here because it includes the setup command that we
+    run to setup our virtualenv and bootstrap it.
+    """
+
+    @staticmethod
+    def devsetup(pkg: str, args: T.Iterable):
+        # First, ensure we have a virtualenv and know where it is
+        VENV = PROJECT_DIR / ".venv"
+        if in_virtualenv():
+            # Already running in the virtual env
+            VENV = Path(sys.prefix)
+            PYTHON = sys.executable
+        elif VENV.exists():
+            # venv exists but we're not running in it
+            PYTHON = get_python_binary_from(VENV)
+        else:
+            print("Creating new virtual environment")
+            PYTHON = create_venv(VENV, prompt=pkg, clear=True)
+
+        print(f"Using virtual environment in {VENV}")
+
+        print("Installing dependencies. May take a bit. Grab a coffee.")
+        _compile_and_sync(PYTHON, args)
+
+        # Finally, make sure we have .env file
+        # If no .env, copy example.env to .env
+        dotenv = PROJECT_DIR / ".env"
+        example = PROJECT_DIR / "example.env"
+        if not dotenv.exists() and example.exists():
+            shutil.copy(example, dotenv)
+
+        print(
+            "Setup complete. Activate your virtual environment, then update your database with:"
+        )
+        print("  python manage.py migrate")
+
+    @staticmethod
+    def pipsync(pkg: str, args: T.Iterable):
+        if not in_virtualenv():
+            print("Activate your virtual environment before running this command.")
+            exit(1)
+        PYTHON = sys.executable
+        _compile_and_sync(PYTHON, args)
+
+
+def django_command():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "writertools.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -126,12 +179,11 @@ def django_command(args=None):
 
 
 if __name__ == "__main__":
-    if sys.argv[1] == "devsetup":
-        rebuild = False
-        if len(sys.argv) > 2 and sys.argv[2] in ["--rebuild", "-r"]:
-            rebuild = True
-        devsetup(rebuild=rebuild)
-    elif sys.argv[1] == "upgrade_requirements":
-        upgrade_requirements()
+    package = get_django_package()
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", f"{package}.settings")
+    command = sys.argv[1]  # argv[0] is manage.py
+    if hasattr(Commands, command):
+        cmd = getattr(Commands, command)
+        cmd(package, args=sys.argv[2:])
     else:
         django_command()
